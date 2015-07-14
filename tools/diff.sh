4b4a0face58ff7b4de48d22dbf1d3b47de4dc05a
Andrew Sutton <andrew.n.sutton@gmail.com>
2015-07-14 15:01:02 -0400

    Comments.

--- tools/diff.sh
+++ tools/diff.sh
@@ -3,5 +3,8 @@
 old=$1
 new=$2
 
+# \providecommand{\DIFadd}[1]{\protect\cbstart{\protect\color{blue}\uwave{#1}}\protect\cbend} %DIF PREAMBLE
+# \providecommand{\DIFdel}[1]{\protect\cbdelete{\protect\color{magenta}\sout{#1}}\protect\cbdelete} %DIF PREAMBLE
+
 flags="--flatten --type CULINECHBAR --config PICTUREENV=codeblock"
 latexdiff $flags $old $new
